97f00c58379d1ea508763bc04dc057a35cefe573
Serge Semin <fancer.lancer@gmail.com>
2019-07-07 23:34:27 +0300

    common: Fix coding style
    It's much easier for the code readability to have the K&R coding
    style utilized with. In particular lets separate the loop keywords
    and their statements and discard unnecessary comments.
    
    Signed-off-by: Serge Semin <fancer.lancer@gmail.com>

--- common.c
+++ common.c
@@ -14,6 +14,7 @@
  */
 
 #include "config.h"
+
 #include <netinet/in.h>		/* for htons(3)			*/
 #include <linux/if_packet.h>
 
@@ -28,6 +29,7 @@
 #include <stdlib.h>		/* for exit(3)			*/
 #include <errno.h>
 #include <assert.h>
+
 #include "linkloop.h"
 
 /* These are used to define different data structs... */
@@ -43,8 +45,8 @@ void dump_packet(struct llc_packet *pack) {
 	size_t len = ntohs(pack->eth_hdr.ether_type);
 
 	printf("PACKET DUMP: data size=%d (0x%x)", len, len);
-	for(i = 0; i < len; i++, p++) {
-		if((i % 16) == 0)
+	for (i = 0; i < len; i++, p++) {
+		if ((i % 16) == 0)
 			printf("\n%04x\t", i);
 		printf("%02x ", (unsigned)*p);
 	}
@@ -54,8 +56,8 @@ void dump_packet(struct llc_packet *pack) {
 char *mac2str(u_int8_t *s) {
 	static char buf[3*ETH_ALEN];
 
-	sprintf (buf, "0x%02X%02X%02X%02X%02X%02X",
-			s[0], s[1], s[2], s[3], s[4], s[5]);
+	sprintf(buf, "0x%02X%02X%02X%02X%02X%02X",
+		s[0], s[1], s[2], s[3], s[4], s[5]);
 	return buf;
 }
 
@@ -63,7 +65,7 @@ int parse_address(u_int8_t mac[], const char *str) {
 	unsigned a, b, c, d, e, f;
 	struct ether_addr ea;
 
-	if(sscanf(str,"%x:%x:%x:%x:%x:%x", &a, &b, &c, &d, &e, &f) == 6) {
+	if (sscanf(str,"%x:%x:%x:%x:%x:%x", &a, &b, &c, &d, &e, &f) == 6) {
 		/* A colon separated notation */
 		mac[0] = (unsigned char) a;
 		mac[1] = (unsigned char) b;
@@ -71,8 +73,7 @@ int parse_address(u_int8_t mac[], const char *str) {
 		mac[3] = (unsigned char) d;
 		mac[4] = (unsigned char) e;
 		mac[5] = (unsigned char) f;
-	} else if(sscanf(str,
-			"0x%02x%02x%02x%02x%02x%02x", &a, &b, &c, &d, &e, &f) == 6) {
+	} else if (sscanf(str, "0x%02x%02x%02x%02x%02x%02x", &a, &b, &c, &d, &e, &f) == 6) {
 		/* Hexadecimal notation (like HPUX) */
 		mac[0] = (unsigned char) a;
 		mac[1] = (unsigned char) b;
@@ -81,7 +82,7 @@ int parse_address(u_int8_t mac[], const char *str) {
 		mac[4] = (unsigned char) e;
 		mac[5] = (unsigned char) f;
 #if HAVE_ETHER_HOSTTON
-	} else if(ether_hostton(str, &ea) == 0) {
+	} else if (ether_hostton(str, &ea) == 0) {
 		/* A name from /etc/ethers */
 		memcpy(mac, ea.ether_addr_octet, 8);
 #endif
@@ -94,7 +95,7 @@ void get_hwaddr(int sock, const char iface[], int *ifindex, u_int8_t mac[]) {
 	struct ifreq ifr;
 
 	strncpy(ifr.ifr_name, iface, IFNAMSIZ);
-	if(ioctl(sock, SIOCGIFHWADDR, &ifr) < 0) {
+	if (ioctl(sock, SIOCGIFHWADDR, &ifr) < 0) {
 		perror("ioctl(SIOCGIFHWADDR)");
 		exit(1);
 	}
@@ -127,7 +128,7 @@ void mk_test_packet(struct llc_packet *pack, struct sockaddr_ll *sll,
 	pack->llc.ssap = (response) ? 0x01 : 0x80;	/* XNS? */
 	pack->llc.ctrl = TEST_CMD;			/* TEST */
 
-	for(i = 0; i < DATA_SIZE(len); i++)
+	for (i = 0; i < DATA_SIZE(len); i++)
 		pack->data[i] = i;
 }
 
@@ -136,15 +137,17 @@ void send_packet(int sock, struct llc_packet *pack, struct sockaddr_ll *sll) {
 
 	/* Send the packet */
 	ret = sendto(sock, pack, FRAME_SIZE(pack), 0, (struct sockaddr *)sll, sizeof(*sll));
-	if(ret == -1) {
+	if (ret == -1) {
 		perror("sendto");
 		exit(1);
 	}
-	if(ret != FRAME_SIZE(pack))
+	if (ret != FRAME_SIZE(pack))
 		fprintf(stderr, "Warning: Incomplete packet sent\n");
-	if(debug_flag)
+
+	if (debug_flag) {
 		printf("sent TEST packet (%d bytes) to %s\n",
 			ntohs(pack->eth_hdr.ether_type), mac2str(pack->eth_hdr.ether_dhost));
+	}
 }
 
 int recv_packet(int sock, struct llc_packet *pack) {
@@ -154,19 +157,21 @@ int recv_packet(int sock, struct llc_packet *pack) {
 
 	len = sizeof(sll);
 	ret = recvfrom(sock, pack, sizeof(*pack), 0, (struct sockaddr *)&sll, &len);
-	if(ret == -1) {
+	if (ret == -1) {
 		if(errno == EINTR)	/* We have a timeout */
 			return 0;
 		perror("recvfrom");
 		exit(1);
 	}
-	if((pack->llc.ctrl & TEST_CMD) != TEST_CMD) {
+
+	if ((pack->llc.ctrl & TEST_CMD) != TEST_CMD) {
 		fprintf(stderr, "got unexpected packet\n");
 		dump_packet(pack);
 		/* continue anyway ! exit(1);*/
 	}
 	ret = ntohs(pack->eth_hdr.ether_type);
-	if(debug_flag)
+	if (debug_flag)
 		printf("received TEST packet (%d bytes) from %s\n", ret, mac2str(pack->eth_hdr.ether_shost));
+
 	return ret;
 }
